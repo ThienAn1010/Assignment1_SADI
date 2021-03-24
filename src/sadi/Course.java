89d627175897e8fae3e814ea07672b33a1ff4a46
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2021-03-24 23:50:52 +0700

    Create Student and Course classes

---
+++ src/sadi/Course.java
@@ -0,0 +1,7 @@
+package sadi;
+
+public class Course {
+    private String id;
+    private String name;
+    private int numberOfCredits;
+}
---
+++ src/sadi/Student.java
@@ -0,0 +1,9 @@
+package sadi;
+
+import java.util.Date;
+
+public class Student {
+    private String id;
+    private String name;
+    private Date birthdate;
+}
