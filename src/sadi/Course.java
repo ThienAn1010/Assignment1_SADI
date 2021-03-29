d89413d9a53146e4823704c0a3651ef25a289891
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2021-03-29 20:47:36 +0700

    POPULATE DATA

--- src/sadi/Course.java
+++ src/sadi/Course.java
@@ -4,4 +4,19 @@ public class Course {
     private String id;
     private String name;
     private int numberOfCredits;
+
+    public Course(String id, String name, int numberOfCredits) {
+        this.id = id;
+        this.name = name;
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", numberOfCredits=" + numberOfCredits +
+                '}';
+    }
 }
--- src/sadi/Student.java
+++ src/sadi/Student.java
@@ -1,9 +1,35 @@
 package sadi;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class Student {
     private String id;
     private String name;
-    private Date birthdate;
+    private LocalDate birthdate;
+
+    public Student(String id, String name, String birthdate) {
+        this.id = id;
+        this.name = name;
+        this.birthdate = birthdateFormatter(birthdate);
+    }
+
+    private LocalDate birthdateFormatter(String birthDateString) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+            LocalDate officialBirthDate = LocalDate.parse(birthDateString, formatter);
+            return officialBirthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
+    }
 }
+
+
+
